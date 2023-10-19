f21881a36fed1ec4f8094e327271a9d879fbeea1
EderMoritz <moritz.eder@liwest.at>
2023-10-19 20:03:28 +0200

    qute beginning

--- src/main/java/at/htlleonding/SomePage.java
+++ src/main/java/at/htlleonding/SomePage.java
@@ -1,13 +1,18 @@
 package at.htlleonding;
 
+import at.htlleonding.control.ArztRepository;
+import at.htlleonding.entity.Arzt;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
 
 import static java.util.Objects.requireNonNull;
 
@@ -16,6 +21,9 @@ public class SomePage {
 
     private final Template page;
 
+    @Inject
+    ArztRepository arztRepository;
+
     public SomePage(Template page) {
         this.page = requireNonNull(page, "page is required");
     }
@@ -23,7 +31,11 @@ public class SomePage {
     @GET
     @Produces(MediaType.TEXT_HTML)
     public TemplateInstance get(@QueryParam("name") String name) {
+
         return page.data("name", name);
+        //List<Arzt> arztList = arztRepository.listAll();
+
+        //return page.data(arztList);
     }
 
 }
